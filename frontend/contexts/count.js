2d890c9bd4b1ebf51f1a69e57566786020cb2f99
Ladrillo <gabriel@cabrejas.net>
2022-04-21 18:56:59 +0200

    committing to "lecture"

--- frontend/contexts/count.js
+++ frontend/contexts/count.js
@@ -3,5 +3,6 @@ import React, { useState, createContext } from 'react'
 export const CountContext = createContext()
 
 export default function CountProvider(props) {
-  // any state you want
+  const [count, setCount] = useState(0)
+  const inc = () => setCount()
 }
