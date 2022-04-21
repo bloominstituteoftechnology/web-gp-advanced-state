220a85156ee3ee3c7235cd02cc6a5837447eefab
Ladrillo <gabriel@cabrejas.net>
2022-04-21 18:35:25 +0200

    committing to "lecture"

--- frontend/components/Todo.js
+++ frontend/components/Todo.js
@@ -2,9 +2,9 @@ import React, { useContext } from 'react'
 import { ColorContext } from '../contexts/colors'
 
 export default function Todo({ todo, toggleStatus }) {
-  const colors = useContext(ColorContext)
+  const { pink } = useContext(ColorContext)
   return (
-    <div onClick={() => toggleStatus(todo.id)} className="todo">
+    <div style={{ color: pink }} onClick={() => toggleStatus(todo.id)} className="todo">
       {todo.name}{todo.completed ? ' ✔️' : ''}
     </div>
   )
