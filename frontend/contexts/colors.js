5d01b4038742dea35d85d88e5c017ec5d8e1ed40
Ladrillo <gabriel@cabrejas.net>
2022-04-21 18:30:40 +0200

    committing to "lecture"

--- frontend/contexts/colors.js
+++ frontend/contexts/colors.js
@@ -3,6 +3,7 @@
 // 3 instantiate a context (and export it)
 // 4 export default a provider
 // 5 go to the top of the tree and USE the provider
+// 6 find a component that wants to consume the data
 
 import React, { createContext } from 'react'
 
