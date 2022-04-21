5e1f9fc4861ad807d9d54de0ed57a29e28c4b38f
Ladrillo <gabriel@cabrejas.net>
2022-04-21 18:43:43 +0200

    committing to "lecture"

---
+++ frontend/contexts/emojis.js
@@ -0,0 +1,6 @@
+// 1 decide what data to teleport
+// 2 pull in the create context helper
+// 3 instantiate a context (and export it)
+// 4 export default a provider
+// 5 go to the top of the tree and USE the provider
+// 6 find a component that wants to consume the data
