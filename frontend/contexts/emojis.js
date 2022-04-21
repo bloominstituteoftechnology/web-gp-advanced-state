f591f966d93506e5d18dd6259dcd4a68214380f7
Ladrillo <gabriel@cabrejas.net>
2022-04-21 18:49:48 +0200

    committing to "lecture"

--- frontend/contexts/emojis.js
+++ frontend/contexts/emojis.js
@@ -4,3 +4,8 @@
 // 4 export default a provider
 // 5 go to the top of the tree and USE the provider
 // 6 find a component that wants to consume the data
+const 
+
+const emoji = {
+  checkmark: '🥰',
+}
