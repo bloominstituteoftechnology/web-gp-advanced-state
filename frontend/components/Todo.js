9a6eb36f8767d89106e533426f08204577256dc0
Ladrillo <gabriel@cabrejas.net>
2022-04-21 18:53:52 +0200

    committing to "lecture"

--- frontend/components/Todo.js
+++ frontend/components/Todo.js
@@ -4,9 +4,10 @@ import { EmojiContext } from '../contexts/emojis'
 
 export default function Todo({ todo, toggleStatus }) {
   const { pink } = useContext(ColorContext)
+  const { checkmark } = useContext(EmojiContext)
   return (
     <div style={{ color: pink }} onClick={() => toggleStatus(todo.id)} className="todo">
-      {todo.name}{todo.completed ? ' ✔️' : ''}
+      {todo.name}{todo.completed ? checkmark : ''}
     </div>
   )
 }
