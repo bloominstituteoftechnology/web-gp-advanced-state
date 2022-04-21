30092c98de88b9e29f800f14100379edb0a9e8a8
Ladrillo <gabriel@cabrejas.net>
2022-04-21 19:04:02 +0200

    committing to "lecture"

--- frontend/components/Todo.js
+++ frontend/components/Todo.js
@@ -1,13 +1,15 @@
 import React, { useContext } from 'react'
 import { ColorContext } from '../contexts/colors'
 import { EmojiContext } from '../contexts/emojis'
+import { CountContext } from '../contexts/count'
 
 export default function Todo({ todo, toggleStatus }) {
   const { pink } = useContext(ColorContext)
   const { checkmark } = useContext(EmojiContext)
+  const { count, inc, dec } = useContext(CountContext)
   return (
     <div style={{ color: pink }} onClick={() => toggleStatus(todo.id)} className="todo">
-      {todo.name}{todo.completed ? checkmark : ''}
+      {todo.name}{todo.completed ? checkmark : ''} {count}
     </div>
   )
 }
